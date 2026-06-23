f003ad1d948a49e7315d668d7a4ed51d6dbfde57
Yashika <adhlakhayashika@gmail.com>
2026-06-23 19:10:28 +0530

    Store questions using dictionaries

--- dsa_tracker.py
+++ dsa_tracker.py
@@ -9,14 +9,27 @@ while True:
     choice = input("Enter choice: ")
 
     if choice == "1":
-        question = input("Enter question name: ")
+        name = input("Enter Question Name: ")
+        topic = input("Enter Topic: ")
+        difficulty = input("Enter Difficulty: ")
+
+        question = {
+            "name": name,
+            "topic": topic,
+            "difficulty": difficulty
+        }
+
         questions.append(question)
+
         print("Question Added!")
 
     elif choice == "2":
         print("\nSolved Questions:")
+
         for i, q in enumerate(questions, start=1):
-            print(f"{i}. {q}")
+            print(f"\n{i}. {q['name']}")
+            print(f"   Topic: {q['topic']}")
+            print(f"   Difficulty: {q['difficulty']}")
 
     elif choice == "3":
         print("Goodbye!")
